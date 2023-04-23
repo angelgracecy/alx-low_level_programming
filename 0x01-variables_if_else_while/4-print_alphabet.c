9a619711204f301cefef0d6dd60808921aa3883f
angelgracecy <angelgracecy@gmail.com>
2023-04-23 06:10:41 -0700

    variables 4

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -Start point
+ * Description: 'Prints alphabets in lowercase followed by a new line'
+ * Return: Always 0
+ */
+int main(void)
+
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+	{
+		if
+			(la != 'e' && la != 'q');
+	}
+	putchar('\n');
+
+	return (0);
+}
