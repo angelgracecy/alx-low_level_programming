2594f243ce3c353c61c32925edd24cd564d7923a
angelgracecy <angelgracecy@gmail.com>
2023-04-23 05:47:21 -0700

    variables 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - start point
+ * Description: 'Prints alphabet in upper and lower case'
+ * Return: Always 0
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+
+	return (0);
+}
