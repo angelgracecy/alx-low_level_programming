dfa7f8e74f750f21d95da498e513f88be396d18f
angelgracecy <angelgracecy@gmail.com>
2023-04-23 08:07:20 -0700

    variables 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - start point
+ * Description: 'print all possible combination of single digit numbers'
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+		putchar(n);
+	if (n != 57)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
