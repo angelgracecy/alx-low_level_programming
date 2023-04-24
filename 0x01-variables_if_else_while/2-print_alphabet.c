017506c2ae9f41c08673bce27e2c3f134c0c8317
angelgracecy <angelgracecy@gmail.com>
2023-04-24 07:47:52 -0700

    variables 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,12 +2,12 @@
 /**
  * main - Start point
  * Description: 'print alphabet in lower case'
- * Return: Always 0 
+ * Return: Always 0
  */
-
 int main(void)
 {
 	char lc;
+
 	for (lc = 'a'; lc <= 'z'; lc++)
 	{
 		putchar(lc);
