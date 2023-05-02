dff4b67f0ce6514c535c0317f860d288db0b2c8c
angelgracecy <angelgracecy@gmail.com>
2023-05-02 16:28:30 -0700

    string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,14 +4,13 @@
  *@s:String pointer to the string whose length is to be found.
  *Return: returns the length of the string.
  */
-
 int _strlen(char *s)
 {
-	int length = 0;
-	"/ incremeant up to when the last character is NULL, '\0'/";
-	while (*(s + length) != 0)
+	int p = 0;
+	incremeant up to when the last character is NULL, \0/
+	while (*(s + p) != 0)
 	{
-		length++;
+		p++;
 	}
-	return (length);
+	return (p);
 }
