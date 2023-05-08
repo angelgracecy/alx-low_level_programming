faada2a8bb043cc426f380c7e0bfb70b011ed011
angelgracecy <angelgracecy@gmail.com>
2023-05-08 10:16:31 -0700

    str

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,21 +8,14 @@
  * @s: string
  * Return: Always 0.
  */
-char *_strchr(char *s, char c);
-{
-while
-	(*s != '\0');
-{
-if
-	(*s == c);
-{
+char *_strchr(char *s, char c) {
+while (*s != '\0') {
+if (*s == c) {
 return (s);
 }
 s++;
 }
-if
-(c == '\0');
-{
+if (c == '\0') {
 return (s);
 }
 return (NULL);
