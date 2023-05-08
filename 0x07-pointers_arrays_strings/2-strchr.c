6cc18fad5dd5500121324e30cd6f80e29fec2d08
angelgracecy <angelgracecy@gmail.com>
2023-05-08 09:39:42 -0700

    str

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,22 +1,24 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * _strncpy - copies a string
- * @dest: destination.
- * @src: source.
- * @n: amount of bytes from src.
- * Return: the pointer to dest.
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-
-char *_strncpy(char *dest, char *src, int n)
+char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
-	for ( ; i < n; i++)
-		dest[i] = '\0';
-
-	return (dest);
-
-
+while (*s != '\0')
+{
+if (*s == c)
+{
+return s;
+}
+s++;
+}
+if (c == '\0')
+{
+return s;
+}
+return (NULL);
 }
