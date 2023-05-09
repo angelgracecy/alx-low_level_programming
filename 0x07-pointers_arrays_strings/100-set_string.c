d3493750a06382f289bcfdb35b84ae51120e18a4
angelgracecy <angelgracecy@gmail.com>
2023-05-09 06:19:58 -0700

    88

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,10 +1,21 @@
-#include "main.h"
-/**
- * set_string - entry point
- * @s: input
- * @to: input
- * Return: 0
- */
+#include <stdio.h>
+
+void set_string(char **s, char *to);
+
+int main(void)
+{
+char *str1 = "hello";
+char *str2 = "world";
+char *str3;
+set_string(&str3, str1); /* set str3 to point to "hello" */
+printf("%s\n", str3); /* prints "hello" */
+
+set_string(&str3, str2); /* set str3 to point to "world" */
+printf("%s\n", str3); /* prints "world" */
+
+return (0);
+}
+
 void set_string(char **s, char *to)
 {
 *s = to;
